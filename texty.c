c177674a1688895569df99ce042a410b5a91a2c1
colinkrogue <122400804+ckrogue@users.noreply.github.com>
2024-08-05 11:30:58 -0600

    added documentation

--- texty.c
+++ texty.c
@@ -10,25 +10,19 @@
 #include <errno.h>
 #include <unistd.h>
 #include <termios.h>
-#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <string.h>
 
 #include "texty.h"
 
-enum Direction {
-  LEFT,
-  RIGHT,
-  UP,
-  DOWN
-};
 struct termios original_termios;
 
 struct cursorCoords {
   int x;
   int y;
 };
+
 struct cursorCoords cursorCoords;
 
 struct windowInformation {
@@ -36,16 +30,19 @@ struct windowInformation {
   int rows;
   int rowOffset;
 };
+
 struct windowInformation windowInformation;
 
 struct row {
   char *text;
   int length;
 };
+
 struct rowList {
   struct row *rows;
   int numRows;
 }; 
+
 struct rowList rowList;
 char *contents = NULL;
 
@@ -63,6 +60,7 @@ void initializeTexty(void) {
 
   FILE *file = fopen("config.txt", "r");
 
+  //handle configuration file
   if (!file) {
     return;
   }
@@ -155,8 +153,6 @@ void handleConfigOptions(char option[3]) {
 //taken from tutorial
 void startRawMode(void) {
     if(tcgetattr(STDIN_FILENO, &original_termios) == -1) error("tcgetattr");
-    //atexit(disableRawMode);
-
     struct termios raw = original_termios;
     raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
@@ -164,7 +160,6 @@ void startRawMode(void) {
     raw.c_oflag &= ~(OPOST);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) error("tcsetattr");
 }
 
@@ -184,15 +179,12 @@ void addRow(char *line, size_t len) {
   memcpy(rowList.rows[index].text, line, len);
   rowList.rows[index].text[len] = '\0';
   rowList.numRows++;
-  //write(STDOUT_FILENO, rowList.rows[index].text, rowList.rows[index].length);
 }
 
 void drawRows(void) {
   int maxNumRoom = log10(rowList.numRows) + 2;
   for(int i = 0; i < windowInformation.rows -1; i++) {
-
     int rowNumber = i + windowInformation.rowOffset;
-    //int rowNumLen = log10(rowList.rows[i].length);
     int rowNumLen;
     if(i == 0) {
       rowNumLen = 1;
@@ -214,7 +206,7 @@ void drawRows(void) {
 }
 
 void displayWelcomeScreen(void) {
-  //rowList = realloc(rowList, sizeof(struct row) * (rowList.numRows + 1));
+  //TODO
 
 }
 void moveCursor(enum Direction direction) {
@@ -314,35 +306,15 @@ void loadFile(char *fileName) {
     }
     addRow(line, len);
   }
-  //write(STDOUT_FILENO, line, 10);
-  
-  
+  fclose(file);
 }
-void handleArguments(int argc, char * argv[]) {
-  for (int i = 1; i < argc; i++) {
-    if(strcmp(argv[i], "red") == 0) {
-      write(STDOUT_FILENO, "\x1b[31m", 5);
-    }
-    else if(strcmp(argv[i], "green") == 0) {
-      write(STDOUT_FILENO, "\x1b[32m", 5);
-    }
-    else if(strcmp(argv[i], "cyan") == 0) {
-      write(STDOUT_FILENO, "\x1b[36m", 5);
-    }
-    else {
-      loadFile(argv[i]);
-    }
-
-  }
-
-}
-
 
 int main(int argc, char *argv[]) {
   initializeTexty();
   startRawMode();
-  handleArguments(argc, argv);
-
+  if(argc >= 2) {
+    loadFile(argv[1]);
+  }
 
   while (1) {
     refreshScreen();
--- texty.h
+++ texty.h
@@ -1,11 +1,59 @@
+#include <stdlib.h>
+
+/// @brief type for storing direction cursor is moving
+enum Direction {
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN
+};
+
+/// @brief puts the terminal into raw input and output mode
+/// @param  void
 void startRawMode(void);
 
-/// @brief quits program with an exit code one. clean exit that returns terminal
-///        to its previous state
-/// @param s message to go before the error code message
+/// @brief quits the program and sends an error message to the terminal
+/// @param s the part of the program causing the error
 void error(const char *s);
 
+/// @brief adds a row of text to the row list
+/// @param line the text to be added
+/// @param len  length of the line to be added
+void addRow(char *line, size_t len);
+
+/// @brief draws all the rows in the row list to the terminal. adds on
+///        row numbers to the front
+/// @param void 
+void drawRows(void);
+
+/// @brief opens a file and puts all of its context into rowList
+/// @param fileName name of the text file
+void loadFile(char *fileName);
+
+/// @brief exits the program cleanly
+/// @param void
+void cleanExit(void);
+
+/// @brief updates the cursors position so that it will be drawn in the righ place
+/// @param direction direction in which the cursor will move
+void moveCursor(enum Direction direction);
+
+/// @brief turns the terminal the appropriate color based on what option is passed in
+/// @param option a command from the config file
+void handleConfigOptions(char option[3]);
+
+/// @brief initializes internal state
+/// @param void
 void initializeTexty(void);
+
+/// @brief displaces the welcome screen NOTE TODO
+/// @param void
 void displayWelcomeScreen(void);
+
+/// @brief Redraws the screen in the terminal
+/// @param void
 void refreshScreen(void);
+
+/// @brief Processes user input from the keyboard and updates internal state
+/// @param void
 void processKeyboardInput(void);
